c8db830365372d09c7c7b26b534cea72ad31d0be
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2022-12-15 08:42:13 -0300

    agregando try with resources para optimizar codigo

--- Conexion.java
+++ Conexion.java
@@ -1,18 +1,25 @@
 package java_pila_ejecucion;
 
-public class Conexion {
+public class Conexion implements AutoCloseable{
 	
 	public Conexion() {
 		System.out.println("Abriendo Conexion");
+		
 	}
 	
 	public void leerDatos() {
 		System.out.println("Recibiendo datos");
-        //throw new IllegalStateException(); //la conexion no fue exitosa, dio un error
+		throw new IllegalStateException(); //la conexion no fue exitosa, dio un error
 	}
 	
 	  public void cerrar() {
           System.out.println("Cerrando conexion ");
       }
+
+	@Override
+	public void close() throws Exception {
+		cerrar();
+		
+	}
 	
 }
--- TestConexion.java
+++ TestConexion.java
@@ -1,18 +1,33 @@
 package java_pila_ejecucion;
 
 public class TestConexion {
-	public static void main(String[] args) {
-		Conexion con = new Conexion();
+	public static void main(String[] args) throws Exception {
+		
+	try (Conexion con = new Conexion()){
+		con.leerDatos();
+	} catch (IllegalStateException ex) {
+		System.out.println("Ejecutando catch");
+		ex.printStackTrace();
+	}
+		
+	}
+	}
+		
+		/*
+		Conexion con = null;
 		try {
+			con = new Conexion();
 			con.leerDatos();			
 		}catch (IllegalStateException ex) {
 			System.out.println("Recibiendo exception");
 			ex.printStackTrace();
 		} finally {
 			System.out.println("Ejecutando finally");
-			con.cerrar();
-		}
+			if (con != null) {
+				con.cerrar();
+			} 
+	  	 }
+		}				
+	}*/
+
 		
-		
-	}
-}
